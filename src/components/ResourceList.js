4ac91af34e1e6fc2e8625721040a7a9af1410f42
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-21 19:29:17 +0200

    apic calls

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class ResourceList extends Component {
+  componentDidUpdate() {
+    axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+  }
   render() {
-    return <div>ResourceList</div>;
+    return <div>{this.props.resource}</div>;
   }
 }
 
